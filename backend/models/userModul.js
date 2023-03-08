6b643914f30f8da9b44beb23274e45529a2a16a9
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-03-09 00:27:09 +0600

    new

---
+++ backend/models/userModul.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        passaword: { type: String, required: true },
+        pi: { type: String, required: true, default: 'https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg' },
+    },
+    {
+        timestamps: true,
+    });
+
+
+const User = mongoose.model("User", userSchema);
+
+//  Export module 
+module.exports = User;
