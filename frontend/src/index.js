9c7273b4f65d7ee8604afcdc26853f3bde711a32
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-03-08 23:31:47 +0600

    This is the new browserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
