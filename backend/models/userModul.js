dc60fa825ac80113b5fcc21ebbd27a92389bd0f7
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-03-09 00:39:22 +0600

    This is the nest

--- backend/models/userModul.js
+++ backend/models/userModul.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema(
     {
         name: { type: String, required: true },
         email: { type: String, required: true },
-        passaword: { type: String, required: true },
+        password: { type: String, required: true },
         pi: { type: String, required: true, default: 'https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg' },
     },
     {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import HomePage from './Pages/HomePage';
 import ChatsPages from './Pages/ChatsPages';
+import "../src/App.css";
 
 const App = () => {
   return (
-    <div>
+    <div className='app'>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/get" element={<ChatsPages />} />
